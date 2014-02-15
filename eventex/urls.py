5a13f78f2eb8fad1834cbea77be6b9868d694260
Mauricio Fagundes <mauricio@cidadelas.com.br>
2014-02-15 19:09:57 -0200

    subscription form

--- eventex/urls.py
+++ eventex/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'eventex.core.views.home', name='home'),
+    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
